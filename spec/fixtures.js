86f1a0ba319213c9ddeddd18c1d79f08ba4a7577
preservance717 <GL717@outlook.com>
2016-07-19 16:21:13 +0800

    add buildCartItem test

---
+++ spec/fixtures.js
@@ -0,0 +1,69 @@
+/**
+ * Created by gaole on 7/19/16.
+ */
+'use strict';
+
+function loadAllItems() {
+    return [
+        {
+            barcode: 'ITEM000000',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+        },
+        {
+            barcode: 'ITEM000001',
+            name: '羽毛球',
+            unit: '瓶',
+            price: 1.00
+        },
+        {
+            barcode: 'ITEM000002',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00
+        },
+        {
+            barcode: 'ITEM000003',
+            name: '苹果',
+            unit: '斤',
+            price: 5.5
+        },
+        {
+            barcode: 'ITEM000004',
+            name: '电池',
+            unit: '个',
+            price: 2.00
+        },
+        {
+            barcode: 'ITEM000005',
+            name: '可口可乐',
+            unit: '瓶',
+            price: 3.00
+        }
+    ];
+}
+
+function loadPromotions() {
+    return [
+        {
+            type: 'BUY_TWO_GET_ONE_FREE',
+            barcodes: [
+                'ITEM000001',
+                'ITEM000005'
+            ]
+        },
+        {
+            type: 'a 95 persent charge',
+            barcodes: [
+                'ITEM000000',
+                'ITEM000003'
+            ]
+        }
+    ];
+}
+
+module.exports = {
+    loadallItems: loadAllItems,
+    loadPromotions: loadPromotions
+}
--- spec/main-test.spec.js
+++ spec/main-test.spec.js
@@ -2,9 +2,86 @@
  * Created by gaole on 7/19/16.
  */
 'use strict';
-describe('test', function () {
-    it('test',function () {
-        var expectNum = 1;
-        expect(test()).toEqual(expectNum);
+
+
+var fixtures = require('./fixtures');
+var printReceipt = require("../main/main.js");
+
+describe('pos', function () {
+    var inputs;
+    var allItems;
+    var promotions;
+
+    beforeEach(function () {
+        inputs = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2',
+            'ITEM000005',
+            'ITEM000005',
+            'ITEM000005'
+        ];
+        allItems = fixtures.loadallItems();
+        promotions = fixtures.loadPromotions();
+    });
+
+    it('should print correct text', function () {
+
+        spyOn(console, 'log');
+
+        printReceipt(inputs);
+
+        const expectText = '***<没钱赚商店>收据***' + '\n' +
+            '名称：羽毛球，数量：5个，单价：1.00(元)，小计：4.00(元)' + '\n' +
+            '名称：苹果，数量：2斤，单价：5.50(元)，小计：11.00(元)' + '\n' +
+            '名称：可口可乐，数量：3个，单价：3.00(元)，小计：6.00(元)' + '\n' +
+            '----------------------' + '\n' +
+            '买二赠一商品' + '\n' +
+            '名称：可口可乐，数量：1瓶' + '\n' +
+            '名称：羽毛球，数量：1个' + '\n' +
+            '**********************' + '\n' +
+            '----------------------' + '\n' +
+            '总计：21.00(元)' + '\n' +
+            '节省：4.00(元)' + '\n' +
+            '**********************';
+
+        expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+
+    it('should print cartItems', function () {
+        var cartItems = buildCartItems(inputs, allItems);
+        var expectCartItems = [
+            {
+                item: {
+                    barcode: "ITEM000001",
+                    name: "羽毛球",
+                    unit: "个",
+                    price: 1.00
+                },
+                count: 5
+            },
+            {
+                item: {
+                    barcode: "ITEM000003",
+                    name: "苹果",
+                    unit: "斤",
+                    price: 5.50
+                },
+                count: 2
+            },
+            {
+                item: {
+                    barcode: "ITEM000005",
+                    name: "可口可乐",
+                    unit: "瓶",
+                    price: 3.00
+                },
+                count: 3
+            }
+        ];
+        expect(cartItems).toEqual(expectCartItems);
     });
 });
