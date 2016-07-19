fcbd8ea19f0b2a3cb6ba4ff652f47cdfca729375
preservance717 <GL717@outlook.com>
2016-07-19 10:00:27 +0800

    travis test1

--- main/main.js
+++ main/main.js
@@ -1,3 +1,6 @@
 /**
  * Created by gaole on 7/19/16.
  */
+function test() {
+    return 1;
+}
--- spec/main-test.spec.js
+++ spec/main-test.spec.js
@@ -1,8 +1,10 @@
 /**
  * Created by gaole on 7/19/16.
  */
+'use strict';
 describe('test', function () {
     it('test',function () {
-        
+        var expectNum = 1;
+        expect(test()).toEqual(expectNum);
     });
 });
