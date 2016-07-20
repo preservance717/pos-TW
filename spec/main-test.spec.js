3f42ed8c31f439b7679bf186e6bce5628962a899
preservance717 <GL717@outlook.com>
2016-07-20 17:55:50 +0800

    format test code

--- spec/main-test.spec.js
+++ spec/main-test.spec.js
@@ -9,6 +9,44 @@ var main = require("../main/main.js");
 
 describe('pos', function () {
     var inputs;
+
+    it('should print text when have both promotions', function () {
+        inputs = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2',
+            'ITEM000005',
+            'ITEM000005',
+            'ITEM000005'
+        ];
+
+        spyOn(console, 'log');
+
+        main.printReceipt(inputs);
+
+        const expectText = '***<没钱赚商店>收据***' + '\n' +
+            '名称：羽毛球，数量：5个，单价：1.00(元)，小计：4.00(元)' + '\n' +
+            '名称：苹果，数量：2斤，单价：5.50(元)，小计：10.45(元)，节省：0.55(元)' + '\n' +
+            '名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：6.00(元)' + '\n' +
+            '----------------------' + '\n' +
+            '买二赠一商品' + '\n' +
+            '名称：羽毛球，数量：1个' + '\n' +
+            '名称：可口可乐，数量：1瓶' + '\n' +
+            '**********************' + '\n' +
+            '----------------------' + '\n' +
+            '总计：20.45(元)' + '\n' +
+            '节省：4.55(元)' + '\n' +
+            '**********************';
+
+        expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+});
+
+describe('unit test', function () {
+    var inputs;
     var allItems;
     var promotions;
     var cartItems;
@@ -112,30 +150,6 @@ describe('pos', function () {
         }
     });
 
-    it('should print text', function () {
-
-        spyOn(console, 'log');
-
-        main.printReceipt(inputs);
-
-        const expectText = '***<没钱赚商店>收据***' + '\n' +
-            '名称：羽毛球，数量：5个，单价：1.00(元)，小计：4.00(元)' + '\n' +
-            '名称：苹果，数量：2斤，单价：5.50(元)，小计：10.45(元)，节省：0.55(元)' + '\n' +
-            '名称：可口可乐，数量：3瓶，单价：3.00(元)，小计：6.00(元)' + '\n' +
-            '----------------------' + '\n' +
-            '买二赠一商品' + '\n' +
-            '名称：羽毛球，数量：1个' + '\n' +
-            '名称：可口可乐，数量：1瓶' + '\n' +
-            '**********************' + '\n' +
-            '----------------------' + '\n' +
-            '总计：20.45(元)' + '\n' +
-            '节省：4.55(元)' + '\n' +
-            '**********************';
-
-        expect(console.log).toHaveBeenCalledWith(expectText);
-    });
-
-    
     it('should print cartItems', function () {
         var cartItems = main.buildCartItems(inputs, allItems);
         var expectCartItems = [
